288a609bc3f3be3c41d266a0e48f57b66555629d
NDQ <103760903+NguyenDuongQuang@users.noreply.github.com>
2025-04-22 00:31:13 +0700

    microservice_get_way_admin_server_monitor

--- orderservice/src/main/java/com/example/orderservice/entity/Order.java
+++ orderservice/src/main/java/com/example/orderservice/entity/Order.java
@@ -1,4 +1,64 @@
 package com.example.orderservice.entity;
 
-public class Order {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = "hoa_don")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Order extends Base{
+
+    @Column(name = "ma_hoa_don", columnDefinition = "nvarchar(256) null")
+    private String maHoaDon;
+
+    @Column(name = "ghi_chu", columnDefinition = "nvarchar(50) null")
+    private String ghiChu;
+
+    @Column(name = "nguoi_nhan", columnDefinition = "nvarchar(256) null")
+    private String nguoiNhan;
+
+    @Column(name = "email_nguoi_nhan", columnDefinition = "nvarchar(256) null")
+    private String emailNguoiNhan;
+
+    @Column(name = "sdtnguoi_nhan", columnDefinition = "nvarchar(50) null")
+    private String SDTNguoiNhan;
+
+    @Column(name = "dia_chi_giao_hang", columnDefinition = "nvarchar(256) null")
+    private String diaChiGiaoHang;
+
+    @Column(name = "tien_ship", columnDefinition = "int null")
+    private int tienShip;
+
+    @Column(name = "tien_giam", columnDefinition = "int null")
+    private int tienGiam;
+
+    @Column(name = "tong_tien_don_hang", columnDefinition = "int null")
+    private int tongTienDonHang;
+
+    @Column(name = "tong_tien_hoa_don", columnDefinition = "int null")
+    private int tongTienHoaDon;
+
+    @Column(name = "loai_hoa_don", columnDefinition = "int null")
+    private int loaiHoaDon;
+
+//    @ManyToOne
+//    @JoinColumn(name = "trang_thai_id", referencedColumnName = "id")
+//    private TrangThai trangThai;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "khach_hang_id", referencedColumnName = "id")
+//    private KhachHang khachHang;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "nhan_vien_id", referencedColumnName = "id")
+//    private NhanVien nhanVien;
+
 }
--- orderservice/src/main/java/com/example/orderservice/entity/Order_Detail.java
+++ orderservice/src/main/java/com/example/orderservice/entity/Order_Detail.java
@@ -1,4 +1,35 @@
 package com.example.orderservice.entity;
 
-public class Order_Detail {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = "hoa_don_chi_tiet")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Order_Detail extends Base{
+
+    @Column(name = "don_gia")
+    private int donGia;
+
+    @Column(name = "so_luong")
+    private int soLuong;
+
+    @Column(name = "thanh_tien")
+    private int thanhTien;
+
+    @JoinColumn(name = "hoa_don_id",referencedColumnName = "id")
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Order order;
+
+    @JoinColumn(name = "san_pham_chi_tiet_id",referencedColumnName = "id")
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Product_Detail productDetail;
 }
